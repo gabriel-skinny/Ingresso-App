4fb69ac67788d72cdc22e395cb7146e1ab835115
gabriel-skinny <gabrielcatoni12@gmail.com>
2021-12-11 18:11:10 -0300

    Adding new funcionalites to routes

--- src/routes.ts
+++ src/routes.ts
@@ -1,4 +1,6 @@
 import { Router } from "express";
+import { ListarSalaController } from "./Controllers/ListarSalaController";
+import { RegistrarAssentoController } from "./Controllers/RegistrarAssentoController";
 import { RegistrarFilmeController } from "./Controllers/RegistrarFilmeController";
 import { RegistrarSalaController } from "./Controllers/RegistrarSalaController";
 import { SessaoUsuarioController } from "./Controllers/SessaoUsuarioController";
@@ -13,10 +15,13 @@ router.get("/user", new UsuarioController().list);
 
 router.post("/login", new SessaoUsuarioController().handle);
 
+router.get("/sala/:numero", new ListarSalaController().handle);
+
 router.use(checkAuth);
 router.use(checkAdmin);
 
 router.post("/filme", new RegistrarFilmeController().handle);
 router.post("/sala", new RegistrarSalaController().handle);
+router.post("/assento", new RegistrarAssentoController().handle);
 
 export { router };
